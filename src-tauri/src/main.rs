a499fcc895bb7692a2da50094e9e1e9a69cc5a32
superevil6 <sevi66@protonmail.com>
2026-07-15 23:08:34 -0500

    Several audio type additions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init()) // native Save dialog for WAV export
         .plugin(tauri_plugin_fs::init())     // write the chosen file to disk
+        .plugin(tauri_plugin_drag::init())   // drag a rendered WAV out into the OS / an engine
         .run(tauri::generate_context!())
         .expect("error while running crunchyfx");
 }
